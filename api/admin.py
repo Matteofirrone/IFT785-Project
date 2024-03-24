f0614ded8c6a9d08de3bec368039fb8c758c86a4
Razafimiandrisoa Véronique <noarimalalav@gmail.com>
2024-03-24 15:46:34 -0400

    create models and view and add json POST data in database

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from api.models import SensorAlert, Person, Caregiver, CaregiverLevel
+
+
+class SensorAlertAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'start', 'location', 'state', 'measurable', 'home')
+
+
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+class CaregiverAdmin(admin.ModelAdmin):
+    pass
+
+
+class CaregiverLevelAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(SensorAlert, SensorAlertAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Caregiver, CaregiverAdmin)
+admin.site.register(CaregiverLevel, CaregiverLevelAdmin)
+
--- api/models.py
+++ api/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.first_name}'
+
+
+class CaregiverLevel(models.Model):
+    level = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.level}'
+
+
+class Caregiver(models.Model):
+    elderly = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='elderly')
+    caregiver = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='caregivers')
+    level = models.ForeignKey(CaregiverLevel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.level} - {self.caregiver}'
+
+
+class SensorAlert(models.Model):
+    subject = models.CharField(max_length=50)
+    start = models.DateTimeField()
+    location = models.CharField(max_length=50)
+    state = models.DecimalField(max_digits=5, decimal_places=2)
+    measurable = models.CharField(max_length=50)
+    home = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.home}'
