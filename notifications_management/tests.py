913f72c17b3b28143dc7c457fd1b6475eec76646
ClemREV0506 <clement.revaud.85@gmail.com>
2024-04-03 14:01:41 -0400

    Add tests NotificationSender

--- notifications_management/tests.py
+++ notifications_management/tests.py
@@ -2,9 +2,13 @@ from datetime import datetime
 from unittest.mock import patch
 
 from django.test import TestCase
+from django.urls import reverse
+
 from api.models import Notification, Person, CaregiverLevel, Caregiver, SensorAlert, Home
 from notifications_management.notification_sender.email_notification_sender import EmailNotificationSender
 from ift785_project import settings
+from notifications_management.notification_sender.notification_sender import NotificationSender
+
 
 class EmailNotificationSenderTestCase(TestCase):
     def setUp(self):
@@ -76,3 +80,45 @@ class EmailNotificationSenderTestCase(TestCase):
             recipient_list=[recipient],
             fail_silently=False,
         )
+
+class NotificationSenderTestCase(TestCase):
+
+    @patch('notifications_management.notification_sender.notification_sender.NotificationSender.send')
+    def test_deliver_notification(self, mock_send):
+        # Créez une instance de NotificationSender (c'est une classe abstraite, donc nous utilisons une sous-classe fictive)
+        class TestNotificationSender(NotificationSender):
+            def generate_content(self, notification):
+                return "Test Content"
+
+            def generate_subject(self, notification):
+                return "Test Subject"
+
+            def get_recipient(self, notification):
+                return "test@example.com"
+
+        notification_sender = TestNotificationSender()
+
+        # Créez un objet de notification
+        notification = Notification()
+
+        # Appelez la méthode deliver_notification avec l'objet de notification
+        notification_sender.deliver_notification(notification)
+
+        # Vérifiez si la méthode send a été appelée avec les bons arguments
+        mock_send.assert_called_once_with(subject="Test Subject", content="Test Content", recipient="test@example.com")
+
+    @patch('notifications_management.notification_sender.notification_sender.reverse')
+    def test_generate_link(self, mock_reverse):
+        # Paramètres de test
+        notification = Notification()
+        token = "test_token"
+        notification.token = token
+        domain = settings.DOMAIN
+        expected_url = "/confirm_notification"  # Supposons que c'est l'URL attendue pour la confirmation de notification
+        mock_reverse.return_value = expected_url
+
+        # Appelez la méthode generate_link sans passer d'objet Notification
+        generated_link = NotificationSender.generate_link(notification)
+
+        # Vérifiez si l'URL générée est correcte
+        self.assertEqual(generated_link, f"{domain}{expected_url}?token={token}")
