61aa2c7d83cd077d06259a730847e4e2c9a8bcf1
ClemREV0506 <clement.revaud.85@gmail.com>
2024-04-08 22:23:29 -0400

    Update NotificationLevels

--- notifications_management/notification_level/notification_level_one.py
+++ notifications_management/notification_level/notification_level_one.py
@@ -1,13 +1,32 @@
 from api.models import Notification
 from notifications_management.notification_level.notification_level import NotificationLevel
+from notifications_management.notification_sender.notification_sender import NotificationSender
 
 
 class NotificationLevelOne(NotificationLevel):
 
     def generate_content(self, notification: Notification):
-        #TODO generate content for Notification with Level = 1
-        return "This is teh content for NotificationLevelOne"
+        elderly = notification.caregiver.elderly
+        sensor_alert = notification.sensor_alert
+
+        content = (
+            f"Dear {elderly.first_name} {elderly.last_name},\n\n"
+            f"We request your attention because a new alert has been triggered. \n"
+            f"Details : "
+            f"Start: {sensor_alert.start}\n"
+            f"Location: {sensor_alert.location}\n"
+            f"State: {sensor_alert.state}\n"
+            f"Measurable: {sensor_alert.measurable}\n\n"
+            f"Please click on the following link to confirm "
+            f"that you have received this notification"
+            f"Confirmation link: {NotificationSender.generate_link(notification)}\n\n"
+            f"Best regards,\n"
+            f"IFT785 Project Team"
+        )
+        return content
 
     def generate_subject(self, notification: Notification):
-        # TODO generate subject for Notification with Level = 1
-        return "This is the subject for NotificationLevelOne"
+        sensor_alert = notification.sensor_alert
+        elderly = notification.caregiver.elderly
+        subject = f"Alert detected for {elderly.first_name} {elderly.last_name} ({sensor_alert.location})"
+        return subject
